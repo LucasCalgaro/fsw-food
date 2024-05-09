776068fbabce0b75ae06877bc0ddb2b1fa96b924
Lucas Eduardo Calgaro <51975452+LucasCalgaro@users.noreply.github.com>
2024-05-09 20:20:42 -0300

    chore: add seed scripts

--- prisma/migrations/20240509230536_init_database/migration.sql
+++ prisma/migrations/20240509230536_init_database/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "Restaurant" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "imageUrl" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
     "deliveryFee" DECIMAL(10,2) NOT NULL,
     "deliveryTimeMinutes" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -26,6 +27,7 @@ CREATE TABLE "Category" (
 CREATE TABLE "Product" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
     "imageUrl" TEXT NOT NULL,
     "price" DECIMAL(10,2) NOT NULL,
     "discountPercentage" INTEGER NOT NULL DEFAULT 0,
