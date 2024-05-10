7330518db5a225ce23f434301225975fba512713
Lucas Eduardo Calgaro <51975452+LucasCalgaro@users.noreply.github.com>
2024-05-09 22:11:52 -0300

    feat: add initial promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -11,6 +12,17 @@ export default function Home() {
       </div>
       <div className="px-5 py-4">
         <CategoryList />
+      </div>
+      <div className="px-5 py-4">
+        <Image
+          src="/images/promo-banner-01.png"
+          alt="Até 30% de Desconto"
+          width={0}
+          height={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   );
