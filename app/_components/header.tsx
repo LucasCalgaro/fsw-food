cd6d74b19b9f4605be2769a5dbcad6b76861e6dc
Lucas Eduardo Calgaro <51975452+LucasCalgaro@users.noreply.github.com>
2024-05-12 19:01:20 -0300

    fix: header logo and link

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="flex justify-between px-5 pt-6">
-      <Image src="/logo.png" alt="FSW Foods" width={110} height={30} />
-      <Button variant="outline" size={"icon"}>
+      <Link href={"/"}>
+        <Image src="/logo.png" alt="FSW Foods" width={128} height={39} />
+      </Link>
+      <Button variant="outline" size={"icon"} className="border-none">
         <MenuIcon />
       </Button>
     </div>
