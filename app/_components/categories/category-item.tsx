35ed37c6ff3aa34dc369c062161a4d5fd7e747dc
Lucas Eduardo Calgaro <51975452+LucasCalgaro@users.noreply.github.com>
2024-05-22 21:21:58 -0300

    feat: add category page

--- app/_components/categories/category-item.tsx
+++ app/_components/categories/category-item.tsx
@@ -1,5 +1,6 @@
 import { Category } from "@prisma/client";
 import Image from "next/image";
+import Link from "next/link";
 
 interface CategoryItemProps {
   category: Category;
@@ -7,17 +8,19 @@ interface CategoryItemProps {
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
   return (
-    <div className="flex items-center justify-center gap-3 rounded-full px-10 py-2 shadow-md">
-      <Image
-        src={category.imageUrl}
-        alt={category.name}
-        width={30}
-        height={30}
-      />
-      <span className="inline-block text-sm font-semibold">
-        {category.name}
-      </span>
-    </div>
+    <Link href={`/categories/${category.id}/products`}>
+      <div className="flex min-h-16 items-center justify-center gap-3 rounded-full px-10 py-2 shadow-md">
+        <Image
+          src={category.imageUrl}
+          alt={category.name}
+          width={30}
+          height={30}
+        />
+        <span className="inline-block text-sm font-semibold">
+          {category.name}
+        </span>
+      </div>
+    </Link>
   );
 };
 
--- app/_components/products/product-item.tsx
+++ app/_components/products/product-item.tsx
@@ -6,6 +6,7 @@ import {
 } from "../../_lib/_helpers/price";
 import Link from "next/link";
 import DiscountBadge from "../discount-badge";
+import { cn } from "@/app/_lib/utils";
 
 interface ProductItemProps {
   product: Prisma.ProductGetPayload<{
@@ -17,40 +18,43 @@ interface ProductItemProps {
       };
     };
   }>;
+  className?: string;
 }
-const ProductItem = ({ product }: ProductItemProps) => {
+
+const ProductItem = ({ product, className }: ProductItemProps) => {
   return (
-    <Link href={`/products/${product.id}`} className="w-[150px] min-w-[150px]">
-      <div className="w-[150px] min-w-[150px] space-y-2">
-        <div className="relative h-[150px] w-full">
-          <Image
-            src={product.imageUrl}
-            alt={product.name}
-            fill
-            className="rounded-lg object-cover shadow-md"
-          />
+    <Link
+      href={`/products/${product.id}`}
+      className={cn("min-w-[150px] space-y-2", className)}
+    >
+      <div className="relative aspect-square w-full">
+        <Image
+          src={product.imageUrl}
+          alt={product.name}
+          fill
+          className="rounded-lg object-cover shadow-md"
+        />
+        {product.discountPercentage > 0 && (
+          <div className="absolute left-2 top-2">
+            <DiscountBadge product={product} />
+          </div>
+        )}
+      </div>
+      <div>
+        <h2 className="truncate text-sm">{product.name}</h2>
+        <div className="flex items-center gap-1">
+          <h3 className="font-semibold">
+            {formatCurrency(calculateProductTotalPrice(product))}
+          </h3>
           {product.discountPercentage > 0 && (
-            <div className="absolute left-2 top-2">
-              <DiscountBadge product={product} />
-            </div>
+            <span className="text-xs text-muted-foreground line-through">
+              {formatCurrency(Number(product.price))}
+            </span>
           )}
         </div>
-        <div>
-          <h2 className="truncate text-sm">{product.name}</h2>
-          <div className="flex items-center gap-1">
-            <h3 className="font-semibold">
-              {formatCurrency(calculateProductTotalPrice(product))}
-            </h3>
-            {product.discountPercentage > 0 && (
-              <span className="text-xs text-muted-foreground line-through">
-                {formatCurrency(Number(product.price))}
-              </span>
-            )}
-          </div>
-          <p className="text-xs text-muted-foreground">
-            {product.restaurant.name}
-          </p>
-        </div>
+        <p className="text-xs text-muted-foreground">
+          {product.restaurant.name}
+        </p>
       </div>
     </Link>
   );
